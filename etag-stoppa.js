529da6d0c59be2c401dc4fea374b46080b35b0da
claustromaniac <20734810+claustromaniac@users.noreply.github.com>
2018-12-25 16:47:12 -0300

    streamlining, console messages

--- etag-stoppa.js
+++ etag-stoppa.js
@@ -1,13 +1,12 @@
-browser.webRequest.onHeadersReceived.addListener(
-	function(d) {
+browser.webRequest.onHeadersReceived.addListener(d => {
 		if (!d.responseHeaders) return;
-		let newHeaders = [];
-		for (var i in d.responseHeaders) {
-			if (d.responseHeaders[i].name.toLowerCase() !== 'etag') {
-				newHeaders.push(d.responseHeaders[i]);
-			}
+		const newHeaders = d.responseHeaders.filter(h => {
+			return h.name.toLowerCase() !== 'etag';
+		});
+		if (newHeaders.length < d.responseHeaders.length) {
+			console.debug(`Removed ETag from request #${d.requestId}\n	${d.url}`);
+			return {responseHeaders: newHeaders};
 		}
-		return {responseHeaders: newHeaders};
 	},
 	{urls: ["<all_urls>"]}, ['blocking', 'responseHeaders']
 );
